fce08c7b6f943f20793d08621bcec7690d40bb23
JoaquinMulet <joaquin.mulet@gmail.com>
2025-03-21 12:47:59 -0300

    Mensaje descriptivo del cambio

--- middleware.js
+++ middleware.js
@@ -1,16 +1,12 @@
-// middleware.js
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Usar clerkMiddleware en su forma más simple
 export default clerkMiddleware();
 
-// Limitar el scope del middleware para reducir potenciales errores
 export const config = {
   matcher: [
-    // Solo rutas de API
-    '/api/(.*)',
-    // Rutas de autenticación
-    '/signin(.*)',
-    '/signup(.*)'
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
