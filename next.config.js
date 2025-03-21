286418a9954dd46205009b8bf08121209913c7e1
JoaquinMulet <joaquin.mulet@gmail.com>
2025-03-21 14:42:49 -0300

    V1.3.2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   // Permitir importaciones con extensiones .jsx
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   // Si experimentas problemas de ESM con node-fetch, utiliza estos transpilePackages
